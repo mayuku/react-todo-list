7c089bb1f564241536e60585f6cfb40c8ba0401e
mayflortadena <mayflor.tadena@smartstart.us>
2022-01-12 22:26:08 +0800

    Convert app component function to arrow function

--- src/App.js
+++ src/App.js
@@ -5,16 +5,14 @@ import AppContextProvider from "./providers/AppContextProvider";
 
 import "./App.css";
 
-function App() {
-  return (
-    <AppContextProvider>
-      <div className="App">
-        <h1>Good day buddy!</h1>
-        <h2>What's the plan for today?</h2>
-        <Todos />
-      </div>
-    </AppContextProvider>
-  );
-}
+const App = () => (
+  <AppContextProvider>
+    <div className="App">
+      <h1>Good day buddy!</h1>
+      <h2>What's the plan for today?</h2>
+      <Todos />
+    </div>
+  </AppContextProvider>
+);
 
 export default App;
